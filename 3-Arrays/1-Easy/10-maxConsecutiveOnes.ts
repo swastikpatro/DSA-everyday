01f840a5caf65d191eb1ac51d1bca3d4d1952e9f
swastikpatro <patroswastik.jsdev@gmail.com>
2022-12-07 22:36:42 +0530

    another soln

--- 3-Arrays/1-Easy/10-maxConsecutiveOnes.ts
+++ 3-Arrays/1-Easy/10-maxConsecutiveOnes.ts
@@ -1,25 +1,16 @@
 function findMaxConsecutiveOnes(nums: number[]): number {
-  let i = 0,
-    j = 0,
-    consectiveCount = 0;
+  let consectiveCount = 0;
   let consectiveMaxCount = 0;
-  while (j < nums.length) {
-    if (nums[i] === 1 && nums[j] === 1) {
-      consectiveCount += 1;
-    } else if (nums[j] === 0 && nums[i] === 1) {
-      console.log(i, j);
-      consectiveCount = 0;
-      i = j + 1;
-      console.log(i);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      consectiveCount++;
     } else {
-      // nums[i] = 0 and nums[j] = 0
-      i++;
+      consectiveCount = 0;
     }
 
     if (consectiveCount > consectiveMaxCount) {
       consectiveMaxCount = consectiveCount;
     }
-    j++;
   }
 
   return consectiveMaxCount;
@@ -27,3 +18,35 @@ function findMaxConsecutiveOnes(nums: number[]): number {
 
 console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1])); // 3
 console.log(findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1])); // 2
+
+// OLD : Using two pointers
+
+// function findMaxConsecutiveOnes(nums: number[]): number {
+//   let i = 0,
+//     j = 0,
+//     consectiveCount = 0;
+//   let consectiveMaxCount = 0;
+//   while (j < nums.length) {
+//     if (nums[i] === 1 && nums[j] === 1) {
+//       consectiveCount += 1;
+//     } else if (nums[j] === 0 && nums[i] === 1) {
+//       console.log(i, j);
+//       consectiveCount = 0;
+//       i = j + 1;
+//       console.log(i);
+//     } else {
+//       // nums[i] = 0 and nums[j] = 0
+//       i++;
+//     }
+
+//     if (consectiveCount > consectiveMaxCount) {
+//       consectiveMaxCount = consectiveCount;
+//     }
+//     j++;
+//   }
+
+//   return consectiveMaxCount;
+// }
+
+// console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1])); // 3
+// console.log(findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1])); // 2
